a979a107beeddd8edcba96f5a3de3e8af24236cc
Viccy Grace <viccy.c.grace@gmail.com>
2018-04-11 21:26:46 +0000

    created controller with product info, display in index

--- learn-angular/js/controllers/MainController.js
+++ learn-angular/js/controllers/MainController.js
@@ -1,3 +1,9 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'Top Sellers in Books';
+  $scope.title = 'Best hills for snowboarding';
+  $scope.promo = 'Fernie';
+  $scope.product = {
+    name: 'The Book of Trees',
+    price: 19,
+    pubdate: new Date('2014', '03', '08')
+  };
 }]);

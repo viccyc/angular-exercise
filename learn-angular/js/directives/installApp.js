df4166616502e20404f0c4e95b2d9b90c365d776
Viccy Grace <viccy.c.grace@gmail.com>
2018-04-12 18:13:20 +0000

    added new return option to installApp directive, called link

--- learn-angular/js/directives/installApp.js
+++ learn-angular/js/directives/installApp.js
@@ -4,6 +4,21 @@ app.directive('installApp', function() {
     scope: {
 
     },
-    templateUrl: "js/directives/installApp.html"
+    templateUrl: "js/directives/installApp.html",
+    link: function(scope, element, attrs) {
+      scope.buttonText = "Install",
+      scope.installed = false,
+
+      scope.download = function() {
+        element.toggleClass('btn-active');
+        if(scope.installed) {
+          scope.buttonText = "Install";
+          scope.installed = false;
+        } else {
+          scope.buttonText = "Uninstall";
+          scope.installed = true;
+        }
+      }
+    }
   }
 });

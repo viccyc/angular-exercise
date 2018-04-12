0952e637038b3f947a38544469125e4b1b8e4a15
Viccy Grace <viccy.c.grace@gmail.com>
2018-04-12 21:18:43 +0000

    files for routing exercise. defined app routes in app.js. HomeController fetches photos. Loops through array.

--- learn-angular/js/app.js
+++ learn-angular/js/app.js
@@ -1,3 +1,16 @@
 // var app = angular.module("myApp", []);
 
-var app = angular.module('ForecastApp', []);
+// var app = angular.module('ForecastApp', []);
+
+var app = angular.module('GalleryApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'HomeController',
+      templateUrl: 'views/home.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+  });
+});

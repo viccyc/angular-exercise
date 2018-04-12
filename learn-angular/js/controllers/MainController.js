b8257aee933a39a2ad64aa8999b23809e074bb86
Viccy Grace <viccy.c.grace@gmail.com>
2018-04-12 17:01:41 +0000

    added another fucntion to controller scope. adds 1 to dislikes and displays in view when clicked

--- learn-angular/js/controllers/MainController.js
+++ learn-angular/js/controllers/MainController.js
@@ -7,31 +7,38 @@ app.controller('MainController', ['$scope', function($scope) {
       price: 19,
       pubdate: new Date('2014', '03', '08'),
       cover: 'img/the-book-of-trees.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubdate: new Date('2013', '08', '01'),
       cover: 'img/program-or-be-programmed.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'My sister\'s keeper',
       price: 15,
       pubdate: new Date('2000', '11', '14'),
       cover: 'img/sister.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Patchwork Planet',
       price: 16,
       pubdate: new Date('2000', '11', '14'),
       cover: 'img/sister.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     }
   ];
   $scope.plusOne = function(index) {
     $scope.products[index].likes += 1;
+  };
+  $scope.minusOne = function(index) {
+    $scope.products[index].dislikes += 1;
   }
 }]);

6263ef35b771889dd1dbd6112064f4c7f85626ef
Viccy Grace <viccy.c.grace@gmail.com>
2018-04-12 16:38:09 +0000

    array of objects, using ng-repeat in the view

--- learn-angular/js/controllers/MainController.js
+++ learn-angular/js/controllers/MainController.js
@@ -1,9 +1,18 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'Best hills for snowboarding';
   $scope.promo = 'Fernie';
-  $scope.product = {
-    name: 'The Book of Trees',
-    price: 19,
-    pubdate: new Date('2014', '03', '08')
-  };
+  $scope.products = [
+    {
+      name: 'The Book of Trees',
+      price: 19,
+      pubdate: new Date('2014', '03', '08'),
+      cover: 'img/the-book-of-trees.jpg'
+    },
+    {
+      name: 'Program or be Programmed',
+      price: 8,
+      pubdate: new Date('2013', '08', '01'),
+      cover: 'img/program-or-be-programmed.jpg'
+    }
+  ]
 }]);
